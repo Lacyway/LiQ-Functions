00048e9b79788264993c0b3ac46b3b4da59e3f5b
Lacyway <tim_westring@hotmail.com>
2022-11-22 10:08:35 +0100

    Removed default respawns
    - Removed debug respawns

--- cfg/CfgRespawns.hpp
+++ cfg/CfgRespawns.hpp
@@ -1,19 +1,7 @@
 class Respawns {
 	class Vehicles {
-		class apc1 {
-			variable = "APC1";
-			name = "APC Spawn 1";
-		};
-		class apc2 {
-			variable = "APC2";
-			name = "APC Spawn 2";
-		};
 	};
 
 	class Locations {
-		class testloc {
-			name = "Test Location";
-			location[] = {4102.62,4130.05,0.00143909};
-		};
 	};	
 };
